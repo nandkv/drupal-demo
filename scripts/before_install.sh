4bf69235c172f6b841cfaa1d9195f4be48950eab
nandkv <4360015+nandkv@users.noreply.github.com>
2024-12-13 13:16:48 -0600

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# Install EPEL and REMI repositories
-yum install -y epel-release
-yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+# Update system
+yum update -y
 
-# Enable REMI PHP 8.1 repository
-yum-config-manager --enable remi-php81
+# Install Amazon Linux Extras
+yum install -y amazon-linux-extras
 
-# Install PHP 8.1 and required extensions
+# Enable and install PHP 8.1
+amazon-linux-extras enable php8.1
+yum clean metadata
 yum install -y php php-cli php-common php-mysqlnd php-zip php-gd php-mbstring php-xml php-json
 
 # Verify PHP version
 php -v
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
+# Install specific version of Composer (2.2 LTS)
+curl -sS https://getcomposer.org/installer | php -- --version=2.2.18
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
 
@@ -22,4 +23,3 @@ composer --version
 
 # Restart Apache
 systemctl restart httpd
-
